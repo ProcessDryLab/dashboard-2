b476de0e4589d3c27fe03a10834c8a915de30494
Andrea Burattin <andrea.burattin@gmail.com>
2022-08-25 00:22:15 +0200

    Added upload capabilities

--- src/modals/FileUploadModal.vue
+++ src/modals/FileUploadModal.vue
@@ -1,7 +1,16 @@
 <template>
-	<b-modal centered id="new-file-modal" title="New file" @ok="handleOk">
+	<b-modal centered id="new-file-modal" title="Upload file" @ok="handleOk">
 		<form ref="form" @submit.stop.prevent="handleSubmit">
-			<ValidatedInputText label="File:" error="Upload a file" type="file" v-model="file" />
+			<!-- <ValidatedInputText label="File:" error="Upload a file" type="file" v-model="file" /> -->
+			<b-form-group label="File to upload:">
+				<b-form-file
+					v-model="file"
+					:state="Boolean(file)"
+					placeholder="Choose a file or drop it here..."
+					drop-placeholder="Drop file here..."
+					@change="onChangeFileUpload()"
+				></b-form-file>
+			</b-form-group>
 			<b-form-group label="Repository:">
 				<b-form-select v-model="repository" :options="repositories" required></b-form-select>
 			</b-form-group>
@@ -10,32 +19,29 @@
 </template>
 
 <script>
-import ValidatedInputText from "../widgets/ValidatedInputText.vue";
+import { RepositoryService } from "../services/repository";
+import axios from "axios";
 
 export default {
 	name: "FileUploadModal",
-	components: {
-		ValidatedInputText,
-	},
+
 	data: () => ({
-		file: "",
+		file: null,
 		repository: "",
 		repositories: [],
 	}),
 	mounted() {
-		console.log(this.$store.getters.getHostsRepository[0].name);
 		this.repositories = this.$store.getters.getHostsRepository.map((str) => ({ text: str.name, value: str.name }));
 		this.repository = this.repositories[0].value;
 	},
 	methods: {
 		handleSubmit() {
-			this.$v.$touch();
-			if (this.$v.$anyError) {
-				return;
-			}
-			this.$store.commit("addHost", { type: this.type, host: this.hostname });
-			this.$nextTick(() => {
-				this.$bvModal.hide("new-host-modal");
+			console.log(this.file);
+			const config = { headers: { "Content-Type": "multipart/form-data" } };
+			const payload = new FormData();
+			payload.append("upfile", this.file, config);
+			axios.post(RepositoryService.buildUploadUrl(this.repository), payload).then(() => {
+				console.log("uploaded");
 			});
 		},
 		handleOk(bvModalEvt) {
--- src/services/repository.js
+++ src/services/repository.js
@@ -2,4 +2,8 @@ export class RepositoryService {
 	static buildPingUrl(url) {
 		return url + "/api/v1/system/ping";
 	}
+
+	static buildUploadUrl(url) {
+		return url + "/api/v1/resources";
+	}
 }
